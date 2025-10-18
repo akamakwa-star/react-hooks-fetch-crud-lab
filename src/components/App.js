ee3138b84bc53213faecd9828e74cb814a31817e
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-10-18 11:44:04 +0300

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
-import React, { useState } from "react";
-import AdminNavBar from "./AdminNavBar";
+import React, { useState, useEffect } from "react";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  // Fetch all questions on mount
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then(setQuestions);
+  }, []);
+
+  // POST: Add new question
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  // DELETE: Remove a question
+  function handleDeleteQuestion(deletedId) {
+    const updated = questions.filter((q) => q.id !== deletedId);
+    setQuestions(updated);
+  }
+
+  // PATCH: Update a question's correct answer
+  function handleUpdateQuestion(updatedQuestion) {
+    const updated = questions.map((q) =>
+      q.id === updatedQuestion.id ? updatedQuestion : q
+    );
+    setQuestions(updated);
+  }
 
   return (
-    <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
-    </main>
+    <section>
+      <h1>Quiz Admin Panel</h1>
+      <QuestionForm onAddQuestion={handleAddQuestion} />
+      <QuestionList
+        questions={questions}
+        onDeleteQuestion={handleDeleteQuestion}
+        onUpdateQuestion={handleUpdateQuestion}
+      />
+    </section>
   );
 }
 
